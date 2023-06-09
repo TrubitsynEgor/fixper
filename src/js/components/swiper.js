3d0e5d0862f43258f3d3a3c23ce58c7fafa051b8
TrubitsynEgor <trubitsynwork74@gmail.com>
2023-06-09 19:25:10 +0500

    +Header +Hero -full adaptive + mobile menu

---
+++ src/js/components/swiper.js
@@ -0,0 +1,9 @@
+import {
+  Swiper,
+  Autoplay,
+  EffectFade,
+  Navigation,
+  Pagination,
+  Thumbs,
+} from "swiper";
+Swiper.use([EffectFade, Navigation, Pagination, Thumbs]);
---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+import "./_vendor";
+import vars from "./_vars";
+import "./_functions";
+import "./_components";
+
+("use-strict");
+
+window.addEventListener("scroll", () => {
+  const scroll = window.pageYOffset;
+  if (scroll > 500) {
+    document.querySelector(".header").classList.add("fixed");
+  }
+  if (scroll < 500) {
+    document.querySelector(".header").classList.remove("fixed");
+  }
+});
+
+document.querySelector(".close-svg").addEventListener("click", () => {
+  document.querySelector(".hero__banner").classList.add("close");
+});
