28a755837a2cbda00b418e272197673ddc8e4cdf
TrubitsynEgor <trubitsynwork74@gmail.com>
2023-06-10 22:54:23 +0500

    +fixed header on scroll. +lock scroll on active mobile menu

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,3 +1,2 @@
-import "./components/burger";
 import "./components/swiper";
 import "./components/showCosts";
--- src/js/functions/burger.js
+++ src/js/functions/burger.js
@@ -1,41 +1,42 @@
-// import { disableScroll } from '../functions/disable-scroll';
-// import { enableScroll } from '../functions/enable-scroll';
+// import { disableScroll } from "../functions/disable-scroll";
+// import { enableScroll } from "../functions/enable-scroll";
 
 (function () {
-  const burger = document?.querySelector('[data-burger]');
-  const menu = document?.querySelector('[data-menu]');
-  const menuItems = document?.querySelectorAll('[data-menu-item]');
-  const overlay = document?.querySelector('[data-menu-overlay]');
+  const burger = document?.querySelector("[data-burger]");
+  const menu = document?.querySelector("[data-menu]");
+  const menuItems = document?.querySelectorAll("[data-menu-item]");
+  const overlay = document?.querySelector("[data-menu-overlay]");
 
-  burger?.addEventListener('click', (e) => {
-    burger?.classList.toggle('burger--active');
-    menu?.classList.toggle('nav--active');
+  burger?.addEventListener("click", (e) => {
+    burger?.classList.toggle("burger--active");
+    menu?.classList.toggle("nav--active");
+    document.body.classList.toggle("lock");
 
-    if (menu?.classList.contains('nav--active')) {
-      burger?.setAttribute('aria-expanded', 'true');
-      burger?.setAttribute('aria-label', 'Закрыть меню');
+    if (menu?.classList.contains("nav--active")) {
+      burger?.setAttribute("aria-expanded", "true");
+      burger?.setAttribute("aria-label", "Закрыть меню");
       disableScroll();
     } else {
-      burger?.setAttribute('aria-expanded', 'false');
-      burger?.setAttribute('aria-label', 'Открыть меню');
+      burger?.setAttribute("aria-expanded", "false");
+      burger?.setAttribute("aria-label", "Открыть меню");
       enableScroll();
     }
   });
 
-  overlay?.addEventListener('click', () => {
-    burger?.setAttribute('aria-expanded', 'false');
-    burger?.setAttribute('aria-label', 'Открыть меню');
-    burger.classList.remove('burger--active');
-    menu.classList.remove('nav--active');
+  overlay?.addEventListener("click", () => {
+    burger?.setAttribute("aria-expanded", "false");
+    burger?.setAttribute("aria-label", "Открыть меню");
+    burger.classList.remove("burger--active");
+    menu.classList.remove("nav--active");
     enableScroll();
   });
 
-  menuItems?.forEach(el => {
-    el.addEventListener('click', () => {
-      burger?.setAttribute('aria-expanded', 'false');
-      burger?.setAttribute('aria-label', 'Открыть меню');
-      burger.classList.remove('burger--active');
-      menu.classList.remove('nav--active');
+  menuItems?.forEach((el) => {
+    el.addEventListener("click", () => {
+      burger?.setAttribute("aria-expanded", "false");
+      burger?.setAttribute("aria-label", "Открыть меню");
+      burger.classList.remove("burger--active");
+      menu.classList.remove("nav--active");
       enableScroll();
     });
   });
