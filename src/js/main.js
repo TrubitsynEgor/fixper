6fab8529413ba7ee3ee82d666e354213a8c24693
TrubitsynEgor <trubitsynwork74@gmail.com>
2023-06-10 16:47:40 +0500

    +benefits(+adaptive) +diagnostics(+change block on click) +progress circle custom JS, (percent from span value in html)

--- src/js/main.js
+++ src/js/main.js
@@ -14,7 +14,47 @@ window.addEventListener("scroll", () => {
     document.querySelector(".header").classList.remove("fixed");
   }
 });
-
+// close banner =============
 document.querySelector(".close-svg").addEventListener("click", () => {
   document.querySelector(".hero__banner").classList.add("close");
+});
+//==========================>
+
+//? Вот тут я 40 минут думал как сколько угодно независимых кружков заполнить, оказалось все очень просто!
+const circle = document.querySelectorAll(".progress");
+const percentArray = [];
+document
+  .querySelectorAll(".percent")
+  .forEach((el) => percentArray.push(parseInt(el.innerHTML)));
+
+circle.forEach((el, idx) => {
+  const progressAnimation = () => {
+    let radius = el.getAttribute("r");
+    let circleLength = 2 * Math.PI * radius;
+    el.setAttribute("stroke-dasharray", circleLength);
+    el.setAttribute(
+      "stroke-dashoffset",
+      circleLength - (circleLength * parseInt(percentArray[idx])) / 100
+    );
+  };
+
+  progressAnimation();
+});
+//====================================================>
+
+const diagnosticsBtn = document.querySelectorAll(".diagnostics__btn");
+const diagnosticPanel = document.querySelector(".diagnostics__panel");
+const diagnosticResult = document.querySelector(".diagnostics__result");
+const backLink = document.querySelector(".diagnostics__result-backlink");
+
+const getDiagnosticsResult = () => {
+  diagnosticPanel.classList.add("diagnostics__panel--hide");
+  diagnosticResult.classList.add("diagnostics__result--active");
+};
+diagnosticsBtn.forEach((el) =>
+  el.addEventListener("click", getDiagnosticsResult)
+);
+backLink.addEventListener("click", () => {
+  diagnosticResult.classList.remove("diagnostics__result--active");
+  diagnosticPanel.classList.remove("diagnostics__panel--hide");
 });
